4dd7859b96f6ac9f0885aba0825c084cc1069432
ju-ki <juki.programming@gmail.com>
2023-11-13 23:16:53 +0900

    ルティングまで

---
+++ pages/index.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/post";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi My name is Jukiya. I'm interested in Web Development and Game
+          Development
+        </p>
+        <p>
+          (this is a sample website on{" "}
+          <a href="https://nextjs.org/learn">Our Next.js tutorial</a>)
+        </p>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
